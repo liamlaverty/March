27c64ea363c4788a8de2640918b1023ec5b7a6ac
Liam Laverty <liamlaverty@gmail.com>
2019-03-31 17:50:24 +0100

    draws a single green rectangle

--- src/application/_main.ts
+++ src/application/_main.ts
@@ -1,17 +1,32 @@
-export class Main {
-    constructor() {
+import { CanvasManager } from "./canvas/canvasManager";
 
+export class Main {
+    private canvasManager: CanvasManager;
+    private loopCount: number = 0;
+    constructor() {
+        this.canvasManager = new CanvasManager();
     }
 
     private readonly launchMessage: string = 'Start';
 
-    Run() {
+    Run(timeout: number) {
         // console.log('running');
         this.Start();
+        setInterval(() => {
+            this.Loop();
+            this.loopCount++;
+        }, timeout);
+        
     }
 
     Start(): string {
         console.log(this.launchMessage + ' will now be posted to the document ');
+        this.canvasManager.CreateCanvas();
         return this.launchMessage;
+    }
+    Loop(){
+        console.log('in loop. Rendering ' + this.loopCount);
+        this.canvasManager.DrawCanvas();
+
     }
 }
---
+++ src/application/canvas/canvasManager.ts
@@ -0,0 +1,26 @@
+export class CanvasManager {
+    private theCanvas: HTMLCanvasElement;
+    constructor() {
+
+    }
+
+    CreateCanvas() {
+        this.theCanvas = document.createElement('canvas');
+
+    }
+
+    DrawCanvas() {
+        this.theCanvas.id = 'workingCanvas';
+        this.theCanvas.height = 400;
+        this.theCanvas.width = 400;
+        const ctx = this.GetCanvasContext2D(this.theCanvas);
+        ctx.fillStyle = 'green';
+        ctx.fillRect(10, 10, 100, 100);
+
+        document.body.appendChild(this.theCanvas);
+    }
+
+    private GetCanvasContext2D(theCanvas: HTMLCanvasElement): CanvasRenderingContext2D {
+        return theCanvas.getContext('2d');
+    }
+}
