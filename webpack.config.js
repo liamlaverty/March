71e940cd0051016068cd381723be7545c6fce80d
Liam Laverty <liamlaverty@gmail.com>
2019-04-03 19:00:13 +0100

    updates webpack to have a start cmd

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,9 @@ module.exports = {
     devtool: 'inline-source-map',
     devServer: {
         contentBase: './dist',
+        watchOptions: {
+            ignored: /node_modules/
+        }
         // compress: true,
         // port: 9000
     },
